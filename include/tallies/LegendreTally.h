d824c08b7b94eae987c4cec6ec11748f9902df14
Nathan Glaser <nglaser3@ajnovak2-serv-01.npre.illinois.edu>
2025-03-02 22:40:31 -0600

    LegendreTally Store Results #1063

--- include/tallies/LegendreTally.h
+++ include/tallies/LegendreTally.h
@@ -21,6 +21,7 @@
 #include "TallyBase.h"
 #include "OpenMCCellAverageProblem.h"
 #include "openmc/tallies/filter_sptl_legendre.h"
+#include "FunctionSeries.h"
 
 class LegendreTally : public TallyBase
 {
@@ -33,6 +34,10 @@ class LegendreTally : public TallyBase
 
         virtual void resetTally() override;
 
+        virtual void computeSumAndMean() override;
+
+
+
         /**
          * spatialFilter is marked as pure virtual, so must be overriden
          * return 0 and null ptr just to satisfy override
@@ -43,15 +48,23 @@ class LegendreTally : public TallyBase
         virtual void resetTally() override;
 
     protected:
+        virtual Real storeResultsInner(const std::vector<unsigned int> & var_numbers,
+                                       unsigned int local_score,
+                                       unsigned int global_score,
+                                       std::vector<xt::xtensor<double, 1>> tally_vals,
+                                       bool norm_by_src_rate = true) override;
+
         std::pair<unsigned, std::vector<openmc::Filter *>> spatialLegendreFilter();
 
         static template <typename T> 
         void setLegendreParams(openmc::SpatialLegendreFilter * filter);
 
+        std:vector<FunctionSeries*> _functions;
         std::vector<unsigned int> _orders;
         Point _min;
         Point _max;
 
-        std::vector<openmc::SpatialLegendreFilter *> _legendre_filter;
-        std::vector<unsigned> _filter_ids;
+        std::vector<Real> _coefficients;
+
+
 }
--- src/tallies/LegendreTally.C
+++ src/tallies/LegendreTally.C
@@ -31,11 +31,14 @@ LegendreTally::validParams()
                             "expansion tallies.");
     params.addRequiredParam<std:vector<unsigned int>>("orders",
                             "The orders (x, y, z) for the Legendre expansions "
-                            "in each dimension.")
+                            "in each dimension.");
     params.addRequiredParam<Point>("minimum",
                             "The minimum bounds (x, y, z) for the bounding box.");
     params.addRequiredParam<Point>("maximum",
                             "The maximum bounds (x, y, z) for the bounding box.");
+    params.addRequiredParam<std::string>("function_suffix","_function",
+                            "The suffix to append to the score as the name of the"
+                            "function object holding the Legendre expansion.");
     return params;
 }
 
@@ -43,8 +46,14 @@ LegendreTally::LegendreTally(const InputParameters & parameters)
 : TallyBase(parameters),
   _orders(getParam<std::vector<unsigned int>>("orders")),
   _min(getParam<Point>("minimum")),
-  _max(getParam<Point>("maximum"))
+  _max(getParam<Point>("maximum")),
+  _name(getParam<std::string>("function_suffix"))
 {
+    for (auto score : _tally_name)
+    {
+      _functions.pushback(this->getFunctionSeries(score + _name))
+    }
+    
     /**
      * OpenMC spatial FETs only support the collision estimator
      */
@@ -55,7 +64,9 @@ LegendreTally::LegendreTally(const InputParameters & parameters)
                     "Collision estimators are currently the only compatible "
                     "estimator type for Spatial Legendre expansion tallies!");
     }
-    return;
+    else
+      _estimator = openmc::TallyEstimator::COLLISION;
+
 }
 
 void 
@@ -110,6 +121,68 @@ LegendreTally::resetTally()
   };
 }
 
+void
+LegendreTally::computeSumAndMean()
+{
+  for (unsigned int score = 0; score < _tally_score.size(); ++score)
+  {
+    first_moment = 1.0;
+    int coeff_index = 0;
+
+    for (int index = 0; index < 3; index++)
+    {
+      first_moment *= _functions[score]->operator[](coeff_index);
+      coeff_index += _orders[score * 3 + index];
+    }
+    _local_sum_tally[score] = first_moment;
+    _local_mean_tally[score] = first_moment / 8; //2 in each direction
+  };
+  
+}
+
+Real
+LegendreTally::storeResultsInner(const std::vector<unsigned int> & var_numbers,
+                                 unsigned int local_score,
+                                 unsigned int global_score,
+                                 std::vector<xt::xtensor<double, 1>> tally_vals,
+                                 bool norm_by_src_rate = true)
+{
+    this->setCoefficients(tally_vals, local_score);
+
+    first_moment = 1.0;
+    int coeff_index = 0;
+
+    for (int index = 0; index < 3; index++)
+    {
+      //getting first moment (first coefficient)
+      first_moment *= _functions[local_score]->operator[](coeff_index);
+      coeff_index += _orders[local_score * 3 + index];
+    }
+    
+    return first_moment;
+}
+
+Real 
+LegendreTally::setCoefficients(std::vector<xt::xtensor<double, 1>> tally_vals, unsigned int score_id)
+{ 
+  /**
+   * Not sure how coeffs are stored by moose function, how to differentiate between x, y, and z?
+   */
+  _coefficients.clear();
+  for (int dim = 0; dim < 3; dim++)
+  {
+    xt::tensor<double, 1> axis = tally_vals[score_id + i];
+    for (int n = 0; n < axis; n++)
+    {
+      // normalization of legendre is 2n+1 /2, openmc doesn't do this
+      _coefficients.push_back(dynamic_cast<Real>(tally_vals[dim]) * (2*n + 1) / 2);
+    }
+  }
+  
+  _functions[score_id]->setCoefficients(_coefficients);
+}
+
+
 std::pair<unsigned, std::vector<openmc::Filter *>>
 LegendreTally::spatialLegendreFilter()
 {
@@ -136,6 +209,14 @@ LegendreTally::setLegendreParams(openmc::SpatialLegendreFilter * filter)
     filter->set_axis(T);
     filter->set_minmax(_min(T), _max(T));
     filter->set_order(_orders(T));
+}
+
+FunctionSeries*
+LegendreTally::getFunctionSeries(std::string name)
+{
+  std::vector<Real> _bounds{_min(0), _max(0), _min(1), _max(1), _min(2), _max(2)};
+
+  return _openmc_problem.makeFunctionSeries(name, "Cartesian", _orders, _bounds);
 }
 #endif
 
